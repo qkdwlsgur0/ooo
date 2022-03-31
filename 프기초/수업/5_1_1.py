fdf64f70e356df0f05c5f45a96e75103d38b8e2b
qkdwlsgur0 <04babo1117@gmail.com>
2022-03-31 14:56:12 +0900

    ooo

--- 프기초/수업/5_1_1.py
+++ 프기초/수업/5_1_1.py
@@ -1,7 +1,29 @@
-def isleapyear(n):
-    if (n >= 0):
-        return n%4 == 0 and n%100 != 0 or n%400 == 0
+from operator import truediv
+
+
+def isleapyear1(y):
+    if (y >= 0):
+        return y%4 == 0 and y%100 != 0 or y%400 == 0
     else:
         return None
         
-print(isleapyear(2022))
+def isleapyear2(y):
+    if (y >= 0):
+        if (y%4 == 0):
+            if (y%100 == 0):
+                if (y%400 == 0):
+                    return True
+                else:
+                    return False
+            else:
+                return True
+        else:
+            return False
+    else:
+        return None
+
+print(isleapyear2(2022))
+print(isleapyear2(2000))
+print(isleapyear2(2100))
+print(isleapyear2(2024))
+print(isleapyear2(-2839))
