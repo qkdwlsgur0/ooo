98e4f3189d6ceff19ad372f6e8229b5705b1ebf6
qkdwlsgur0 <04babo1117@gmail.com>
2022-04-04 12:27:06 +0900

    ooo

--- c/ccc.c
+++ c/ccc.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    printf("5+3");
+    int a, b;
+    scanf("%d %d", &a, &b);
+    if (a>b) {
+        printf(">");
+    }
+    else if (a<b) {
+        printf('<');
+    }
+    else {
+        printf('==');
+    }
+     
+    return 0;
 }
